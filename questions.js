b5dd8b7b2ae1228828d1c81fbff3cfe60bf39687
Sarvadnya Yeole <yeolesv1012@gmail.com>
2026-05-28 15:55:34 +0530

    Solved some questions of conditional statements

--- questions.js
+++ questions.js
@@ -10,9 +10,11 @@ const compound_interest = () => {
 
   console.log(i);
 };
+// compound_interest();
 
 //Otp Generator:
 const Otp = () => console.log(Math.floor(Math.random() * 1000000));
+// Otp();
 
 //Area of triangle by heron's formula
 const areat = () => {
@@ -30,6 +32,7 @@ const areat = () => {
     console.log(area.toFixed(3));
   }
 };
+// areat();
 
 //Circumference of Circle
 const circum = () => {
@@ -41,6 +44,7 @@ const circum = () => {
     console.log("Value of radius must be positive.");
   }
 };
+// circum();
 
 //Dice:
 const dice = () => {
@@ -49,3 +53,86 @@ const dice = () => {
     console.log(d);
   }
 };
+// dice();
+
+//Leap Year:
+const leap = () => {
+  let isLeap = false;
+  let year = Number(prompt("Enter the year :-"));
+
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 == 0) isLeap = true;
+      else isLeap = false;
+    } else {
+      isLeap = true;
+    }
+  } else isLeap = false;
+
+  if (isLeap) console.log(year + " is a leap year");
+  else console.log(year + " is not a leap year");
+};
+// leap();
+
+//Leap year:
+const leap2 = () => {
+  let isLeap = false;
+  let year = Number(prompt("Enter the year :-"));
+
+  if (year % 4 == 0 && year % 100 != 0) {
+    isLeap = true;
+  } else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+    isLeap = true;
+  } else isLeap = false;
+
+  if (isLeap) console.log(year + " is a leap year");
+  else console.log(year + " is not a leap year");
+};
+// leap2();
+
+//Shop discount:
+const shop = () => {
+  let amount = Number(
+    prompt("Enter the amount of which products you have purchased: "),
+  );
+  let dis = 0;
+  if (amount <= 5000 && amount >= 0) {
+    dis = 0;
+  } else if (amount <= 7000 && amount >= 5001) {
+    dis = 5;
+  } else if (amount <= 9000 && amount >= 7001) {
+    dis = 10;
+  } else if (amount > 9000) {
+    dis = 20;
+  } else {
+    console.log("Please enter valid amount.");
+  }
+  console.log(
+    "Your payable amount is " +
+      (amount - (amount * dis) / 100) +
+      " after applying " +
+      dis +
+      "% discount.",
+  );
+};
+// shop();
+
+//Electricity bill:
+const electricity = () => {
+  let unit = Number(prompt("Enter the electricity units:- "));
+
+  let amount = 0;
+  if (unit >= 0 && unit <= 100) {
+    amount = unit * 4.2;
+  } else if (unit >= 101 && unit <= 200) {
+    amount = 100 * 4.2 + (unit - 100) * 6;
+  } else if (unit >= 201 && unit <= 400) {
+    amount = 100 * 4.2 + 100 * 6 + (unit - 200) * 8;
+  } else if (unit >= 401) {
+    amount = 100 * 4.2 + 100 * 6 + 200 * 8 + (unit - 400) * 13;
+  } else {
+    console.log("Enter valid input.");
+  }
+  console.log("Your electricity bill is " + amount + "₹");
+};
+electricity();
